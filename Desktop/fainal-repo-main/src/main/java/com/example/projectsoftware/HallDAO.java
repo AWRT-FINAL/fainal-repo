eeb51d84d5e7480aea9d35cc6540cd2694c04db1
Aseelmaradwi <kaseel134@gmail.com>
2024-03-31 23:06:07 +0300

    Your descriptive commit message

---
+++ Desktop/fainal-repo-main/src/main/java/com/example/projectsoftware/HallDAO.java
@@ -0,0 +1,45 @@
+package com.example.projectsoftware;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HallDAO {
+
+    private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "1482003";
+
+    public static String[] getHallsBasedOnBudget(double budget) {
+        List<String> hallsList = new ArrayList<>();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+            String sql = "SELECT hallname FROM software.halls WHERE priceperhour <= ?";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setDouble(1, budget);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                String hallName = resultSet.getString("hallname");
+                hallsList.add(hallName);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) resultSet.close();
+                if (preparedStatement != null) preparedStatement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return hallsList.toArray(new String[0]);
+    }
+}
